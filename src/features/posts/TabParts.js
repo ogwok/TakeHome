b6796235481bf09f02284e8fe4e1c58332bb4643
rumbiiha swaibu <sswaib11@gmail.com>
2022-02-02 08:39:39 +0300

    refactor tabs

--- src/features/posts/TabParts.js
+++ src/features/posts/TabParts.js
@@ -1,4 +1,3 @@
-import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
 export function TabPanel(props) {
@@ -14,7 +13,7 @@ export function TabPanel(props) {
       >
         {value === index && (
           <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
+            {children}
           </Box>
         )}
       </div>
